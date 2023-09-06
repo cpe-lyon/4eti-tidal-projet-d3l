646be7ded11a1baf54e12e3db6c3e27e23747d59
loic-prn <l.perrin@proton.me>
2023-09-06 13:54:34 +0200

    Beginning of routing

--- src/index.php
+++ src/index.php
@@ -1,4 +1,12 @@
 <?php
 
-	echo "string";
-?>
+include_once 'd3l/routing/router.php';
+
+$router = new Router($_SERVER['REQUEST_URI']);
+$router->get('/:id', function($id){ echo 'ton id:' . $id;});
+$router->get('/test/lol', function(){ echo 'lel';});
+
+$router->run();
+
+die();
+echo "string";
