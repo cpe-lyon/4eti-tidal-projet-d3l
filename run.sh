6426f38c9400471cadf6aa1a6a2e074e3ac5d250
Leon Dumestre <leon.dumestre@gmail.com>
2023-10-24 14:14:27 +0200

    refactor: update bd script

--- run.sh
+++ run.sh
@@ -12,17 +12,20 @@ case "$command" in
 # database commands
     "db:ip")
         docker inspect -f '{{range.NetworkSettings.Networks}}{{.IPAddress}}{{end}}' "my-postgres-container"
-        echo "you should use this ip in your config.json file"
         ;;
-    "db:init")
-        docker exec my-php-container php "d3l/database/scripts/init.php"
+    "db:reset")
+        docker exec my-php-container php "d3l/database/scripts/reset.php"
         ;;
     "db:migrate")
         if [ "$args" == "--create" ]; then
-            php "d3l/database/scripts/newMigration.php"
+            php "d3l/database/scripts/create.php"
             exit 0
         fi
-        php "d3l/database/scripts/newMigration.php"
+        if [ "$args" == "--execute" ]; then
+            php "d3l/database/scripts/migrate.php"
+            exit 0
+        fi
+        php "d3l/database/scripts/create.php"
         docker exec my-php-container php "d3l/database/scripts/migrate.php"
         ;;
 
